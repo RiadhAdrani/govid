7d2e08b6212cff67606962439cb579fcfe0c99ea
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-06-25 18:09:46 +0100

    chore: add unocss

---
+++ frontend/unocss.config.ts
@@ -0,0 +1,26 @@
+// uno.config.ts
+import { defineConfig, presetUno } from 'unocss';
+import { presetWebFonts } from 'unocss';
+
+//  const join = (array: Array<string>): string => `@apply ${array.join(' ')}`;
+
+export default defineConfig({
+  presets: [
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        sans: 'Roboto',
+      },
+    }),
+    presetUno(),
+  ],
+  shortcuts: [
+    {
+      col: 'flex flex-col',
+      row: 'flex flex-row',
+
+      'col-center': 'col justify-center items-center',
+      'row-center': 'row justify-center items-center',
+    },
+  ],
+});
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
+import UnoCSS from 'unocss/vite';
+import path from 'path';
 
 export default defineConfig({
+  plugins: [UnoCSS({ configFile: path.resolve(__dirname, './uno.config.ts') })],
   server: {
     port: 3000,
   },
